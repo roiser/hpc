c7825fc59be934c75cda97d87668ef09892a0566
Stefan Roiser <stefan.roiser@cern.ch>
2025-09-09 14:42:09 +0300

    few changes for mgbatch

--- mg5batch.sh
+++ mg5batch.sh
@@ -1,35 +1,24 @@
 #!/bin/sh
 
-HIP_VERSION=
-GCC_VERSION=
-PYTHON_VERSION=3.11.7
-MG5_USER=${SLURM_JOB_USER}
-MG5_HOME=${SLURM_SUBMIT_DIR}
-MG5_SLURM_DIR=slurm-${SLURM_JOBID}
-
 MG5_PROC_DIR=$1
 MG5_RUN_OPTS=$2
 MG5_NUM_EVTS=$3
 MG5_PROC_TAG=$4
 
-#module load rocm/12.6
-#module load gcc/13.2.0
-module load cray-python/${PYTHON_VERSION}
+MG5_ENV=hpc/conf/${MG5_HPC}.sh
+if [[ -e ${MG5_ENV} ]]; then source ${MG5_ENV}; fi
 
+for mod in "${MG5_MODULES}"; do module load ${mod}; done
 
-MG5_OUTPUT=/projappl/${SLURM_JOB_ACCOUNT}/MG5
-if [ ${MG5_PROC_TAG} != "" ]; then MG5_OUTPUT=${MG5_OUTPUT}/${MG5_PROC_TAG}; fi
-MG5_OUTPUT=${MG5_OUTPUT}/${MG5_SLURM_DIR}
-MG5_WORK_DIR=${TMPDIR}/${MG5_SLURM_DIR}
 mkdir -p ${MG5_WORK_DIR}
-mkdir -p ${MG5_OUTPUT}
+mkdir -p ${MG5_OUTP_DIR}
 cd ${MG5_WORK_DIR}
 
 echo ">>>>> MG5_CONF >>>>> PROC_DIR >>>>> ${MG5_PROC_DIR}"
 echo ">>>>> MG5_CONF >>>>> RUN_OPTS >>>>> ${MG5_RUN_OPTS}"
 echo ">>>>> MG5_CONF >>>>> NUM_EVTS >>>>> ${MG5_NUM_EVTS}"
 echo ">>>>> MG5_CONF >>>>> PROC_TAG >>>>> ${MG5_PROC_TAG}"
-echo ">>>>> MG5_CONF >>>>> OUTP_DIR >>>>> ${MG5_OUTPUT}"
+echo ">>>>> MG5_CONF >>>>> OUTP_DIR >>>>> ${MG5_OUTP_DIR}"
 echo ">>>>> MG5_CONF >>>>> WORK_DIR >>>>> ${MG5_WORK_DIR}"
 
 echo ">>>>> MG5_TIME >>>>> START    >>>>> `date`"
@@ -42,11 +31,10 @@ echo ">>>>> MG5_TIME >>>>> UNTAR    >>>>> `date`"
 
 echo ">>>>> MG5_TIME >>>>> RUN      >>>>> `date`"
 
-cp events.lhe.gz ${MG5_OUTPUT}
+cp events.lhe.gz ${MG5_OUTP_DIR}
 
 echo ">>>>> MG5_TIME >>>>> UPLOAD   >>>>> `date`"
 
-tar cvfz . ${MG5_OUTPUT}/${MG5_SLURM_DIR}.tar.gz
+tar cvfz ${MG5_OUTP_DIR}/${MG5_SLURM_DIR}.tar.gz .
 
 echo ">>>>> MG5_TIME >>>>> END      >>>>> `date`"
-
