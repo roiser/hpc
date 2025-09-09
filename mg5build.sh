5e5e1b7d566eeb835d8bb0706914dca575887318
Stefan Roiser <stefan.roiser@cern.ch>
2025-09-09 14:19:05 +0300

    eval build, simplify module loads

--- mg5build.sh
+++ mg5build.sh
@@ -1,12 +1,11 @@
 #!/bin/sh
 
-source ./mg5env.sh
+MG5_ENV=hpc/conf/${MG5_HPC}.sh
+if [[ -e ${MG5_ENV} ]]; then source ${MG5_ENV}; fi
 
-if [[ ${MG5_GPU_MODULE} != "" ]]; then module load ${MG5_GPU_MODULE}/${MG5_GPU_VERSION}; fi
-if [[ ${MG5_GCC_MODULE} != "" ]]; then module load ${MG5_GCC_MODULE}/${MG5_GCC_VERSION}; fi
-if [[ ${MG5_PYTHON_MODULE} != "" ]]; then module load ${MG5_PYTHON_MODULE}/${MG5_PYTHON_VERSION}; fi
+for mod in "${MG5_MODULES}"; do module load ${mod}; done
 
 if [ "x$1" != "x" ]; then MG5_PROC=$1; fi
 
 cd ${MG5_HOME}/sw/madgraph4gpu/MG5aMC/mg5amcnlo
-MADGRAPH_${MG5_GPU_NAME}_ARCHITECTURE=${MG5_GPU_ARCH} ./bin/mg5_aMC cmd_proc/proc_${MG5_PROC}.mg5
+eval MADGRAPH_${MG5_GPU_NAME}_ARCHITECTURE=${MG5_GPU_ARCH} ./bin/mg5_aMC cmd_proc/proc_${MG5_PROC}.mg5
